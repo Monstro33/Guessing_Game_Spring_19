43bb67d087a8e157ebd23c403c97406bf367b615
Michael Manville <monstro33@yahoo.com>
2019-01-16 14:13:27 -0500

    version 2

--- Guessing_Game_Spring_19/Program.cs
+++ Guessing_Game_Spring_19/Program.cs
@@ -6,32 +6,30 @@ namespace Guessing_Game_Spring_19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please Guess A Number Between 1-10");
+            Console.WriteLine("Please Guess A Number Between 1-10 and hit enter to submit");
             string userGuess = Console.ReadLine();
+
             Console.WriteLine("You Guessed: " + userGuess);
-            // if they guess 7
+
             if (userGuess.Equals("7"))
             {
-                // then they win
                 Console.WriteLine("You Win......");
             }
             else if (userGuess.Equals("0"))
             {
-                // then re-state the directions
                 Console.WriteLine("Please Follow the Instructions...Pick A Number Between 1-10");
-            }
-            else
-            {
-                // otherwise they lose
-                Console.WriteLine("You Lose!");
-            }
-            // if they guess 0
-           
 
+                userGuess = Console.ReadLine();
+            }
+
+            else
+
+            {
+                Console.WriteLine("Wrong Number, Try Again!");
+            }         
         }
     }
 }
-
 
 
 
