a4f1fffc2f2ddb66b5e723bdc434a80428925702
Michael Manville <monstro33@yahoo.com>
2019-01-16 13:18:43 -0500

    First Iteration Complete

--- Guessing_Game_Spring_19/Program.cs
+++ Guessing_Game_Spring_19/Program.cs
@@ -6,8 +6,33 @@ namespace Guessing_Game_Spring_19
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Goodbye");
+            Console.WriteLine("Please Guess A Number Between 1-10");
+            string userGuess = Console.ReadLine();
+            Console.WriteLine("You Guessed: " + userGuess);
+            // if they guess 7
+            if (userGuess.Equals("7"))
+            {
+                // then they win
+                Console.WriteLine("You Win......");
+            }
+            else if (userGuess.Equals("0"))
+            {
+                // then re-state the directions
+                Console.WriteLine("Please Follow the Instructions...Pick A Number Between 1-10");
+            }
+            else
+            {
+                // otherwise they lose
+                Console.WriteLine("You Lose!");
+            }
+            // if they guess 0
+           
+
         }
     }
 }
+
+
+
+
+  
